b9967b3bac611d5a379d2ee1f95a0e393d580206
wired87 <derbenedikt.sterra@gmail.com>
2025-04-29 17:10:40 +0200

    update

--- yaml.py
+++ yaml.py
@@ -5,3 +5,9 @@ def load_yaml(filepath) -> dict:
         data = yaml.safe_load(file)
     #print("data", data)
     return data
+
+
+def write_yaml(content:dict, dest):
+    yml_content = yaml.dump(content, default_flow_style=False, sort_keys=False)
+    with open(dest, 'r', encoding="utf-8") as f:
+        f.write(yml_content)
