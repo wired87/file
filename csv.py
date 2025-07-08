4d66151113993e29ef8f388c59fe85fef56deec0
wired87 <derbenedikt.sterra@gmail.com>
2025-07-08 16:22:02 +0200

    rennt

--- csv.py
+++ csv.py
@@ -1,6 +1,8 @@
 import csv
 from io import StringIO, BytesIO
 
+from utils.printer import printer
+
 
 def dict_2_csv(data:list[dict], keys):
     print("Save local scv data")
@@ -10,16 +12,24 @@ def dict_2_csv(data:list[dict], keys):
         writer.writerows(data)
 
 
-def dict_2_csv_buffer(data: list[dict], keys, bbuffer) -> BytesIO:
+def dict_2_csv_buffer(data: list[dict], keys, bbuffer, testing=True, save_dir=None) -> BytesIO:
     """
     Schreibt die CSV in einen BytesIO-Puffer und gibt diesen zurück.
     """
     # StringIO (Text) als Zwischenpuffer
-    str_buf = StringIO()
-    writer = csv.DictWriter(str_buf, fieldnames=keys)
-    writer.writeheader()
-    writer.writerows(data)
-    byte_buf = BytesIO()
-    # In BytesIO konvertieren (UTF-8 Encoding)
-    byte_buf.write(str_buf.getvalue().encode("utf-8"))
-    return byte_buf
+    #printer(locals())
+    clean_data = []
+    if testing is True:
+        with open(save_dir, "w", newline="", encoding="utf-8") as f:
+            writer = csv.DictWriter(f, fieldnames=keys)
+            writer.writeheader()
+            writer.writerows(data)
+    else:
+        str_buf = StringIO()
+        writer = csv.DictWriter(str_buf, fieldnames=keys)
+        writer.writeheader()
+        writer.writerows(data)
+        byte_buf = BytesIO()
+        # In BytesIO konvertieren (UTF-8 Encoding)
+        byte_buf.write(str_buf.getvalue().encode("utf-8"))
+        return byte_buf
