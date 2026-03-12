825d8cf650589a79a7bc4653aa3a256aaca5cc47
bestb <derbenedikt.sterra@gmail.com>
2026-03-12 19:14:30 +0100

    mvp works

--- flatten_dict.py
+++ flatten_dict.py
@@ -1,8 +1,3 @@
-
-
-
-
-
 def flatten_attributes(attributes):
     """Recursively flattens a nested dictionary, handling lists properly."""
     #print("Flatting attrs", attributes)
@@ -14,7 +9,6 @@ def flatten_attributes(attributes):
                 new_key = f"{prefix}_{k}" if prefix else k
                 flattened.update(_flatten(v, new_key))
         elif isinstance(obj, list):
-            # Instead of merging into the same dict, preserve the list structure.
             new_items = []
             for item in obj:
                 if isinstance(item, dict):
@@ -29,30 +23,3 @@ def flatten_attributes(attributes):
 
     return _flatten(attributes)
 
-
-
-
-
-
-
-
-
-"""def flatten_attributes(attributes):
-    Recursively flattens a nested dictionary, handling lists properly.
-    def _flatten(obj, prefix=''):
-        flattened = {}
-
-        if isinstance(obj, dict):
-            for k, v in obj.items():
-                new_key = f"{prefix}_{k}" if prefix else k
-                flattened.update(_flatten(v, new_key))
-
-        elif isinstance(obj, list):
-            list_prefix = ""
-            for item in obj:
-                flattened.update(_flatten(item, list_prefix))
-        else:
-            flattened[prefix] = obj
-        return flattened
-
-    return _flatten(attributes)"""
